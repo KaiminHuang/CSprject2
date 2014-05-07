dd5efbf8f791d273fa51f694f310dd5cec46e93d
Avin Chadee <achadee15@gmail.com>
2014-05-07 20:29:53 +1000

    cleaned up the main

--- proxy-server/server.c
+++ proxy-server/server.c
@@ -7,35 +7,15 @@
 #include "connection.h"
 
 
-#define MAXUSERS 64
-
-/*---
--- Function declarations
----*/
-
 /* configures a new server on startup */
-struct Server config(char * port, int timeout);
+struct Server config(char * port);
 
 /* launches the server */
 void luanchServer (struct Server s);
 
-/* stores a single request */
-struct connectionDetails {
-	char * host;
-	int  * port;
-};
-
-/* individual client details (per connection) */
-struct Client {
-	struct connectionDetails clientDetails;
-	struct connectionDetails requestDetails;
-};
-
 /* new proxy server object */
 struct Server {
    char * port;
-   int timeout;
-   struct Client users[MAXUSERS];
 };
 
 int main(int argc, char **argv)
@@ -46,17 +26,16 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 	//config a server
-	struct Server s = config(argv[1], 500);
+	struct Server s = config(argv[1]);
 	//launch server
 	luanchServer(s);
 
 	return 0;
 }
 
-struct Server config(char * port, int timeout){
+struct Server config(char * port){
 	struct Server newServer;
 	newServer.port = port;
-	newServer.timeout = timeout;
 	return newServer;
 }
 
