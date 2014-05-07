a79f4f639fcf3be4d5c8559eac235e70af29c6df
Kaimin Huang <kaiminhuang709@gmail.com>
2014-05-07 16:09:55 +1000

    Add socket connection on Server side

--- server.c
+++ server.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 /*---
 -- Function declarations
 ---*/
@@ -41,12 +44,67 @@ struct Server {
    struct Client users[MAXUSERS];
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+	int sockfd, newsockfd, portno, clilen;
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
+
+	if (argc < 2) 
+	{
+		fprintf(stderr,"ERROR, no port provided\n");
+		exit(1);
+	}
+	 /* Create TCP socket */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+	{
+		perror("ERROR opening socket");
+		exit(1);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	// Set the port number according to the parameter passed in
+	portno = atoi(argv[1]);
+	/* Create address we're going to listen on (given port number)
+	 - converted to network byte order & any IP address for 
+	 this machine */
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);  // store in machine-neutral format
+	/* Bind address to the socket */
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+			sizeof(serv_addr)) < 0) 
+	{
+		perror("ERROR on binding");
+		exit(1);
+	}
+
+	/* Listen on socket - means we're ready to accept connections - 
+	 incoming connection requests will be queued */
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+	/* Accept a connection - block until a connection is ready to
+	 be accepted. Get back a new file descriptor to communicate on. */
+	newsockfd = accept(	sockfd, (struct sockaddr *) &cli_addr, 
+						&clilen);
+	if (newsockfd < 0) 
+	{
+		perror("ERROR on accept");
+		exit(1);
+	}
+	bzero(buffer,256);
+	/* Read characters from the connection,
+		then process */
+	n = read(newsockfd,buffer,255);
+
+	logger(buffer);
+
+	close(sockfd);
 	//config a server
 	struct Server s = config(1234, 500);
 	//launch server
-	luanchServer(s);
+	// luanchServer(s);
 
 	return 0;
 }
@@ -74,6 +132,7 @@ char * getReadableTime(){
 
 void luanchServer (struct Server s){
 	while(1) {
+
 		// 1. receive connection from client
 		// 2. store connection details
 		//		-- GET, POST
